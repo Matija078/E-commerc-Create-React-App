99481820de6b8b9b3d3557ffc175265ea6486d00
Matija2003 <matijaj@viken.no>
2023-04-08 19:43:19 +0200

    E-commerc-Create-React-App

--- backend/data.js
+++ backend/data.js
@@ -3,14 +3,14 @@ import bcrypt from 'bcryptjs';
 const data = {
   users: [
     {
-      name: 'Matija',
+      name: 'Basir',
       email: 'admin@example.com',
       password: bcrypt.hashSync('123456'),
       isAdmin: true,
     },
     {
       name: 'John',
-      email: 'johan@gmail.com',
+      email: 'user@example.com',
       password: bcrypt.hashSync('123456'),
       isAdmin: false,
     },
