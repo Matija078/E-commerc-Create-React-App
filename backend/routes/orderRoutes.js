2cb687cf4f2d2583dfac16da36a823a9a40c4439
Matija2003 <matijaj@viken.no>
2023-04-10 14:28:20 +0200

    E-commerc-Create-React-App

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -35,6 +35,14 @@ orderRouter.get(
     }
   })
 );
+orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
 orderRouter.put(
   '/:id/pay',
   isAuth,
@@ -54,6 +62,25 @@ orderRouter.put(
       res.send({ message: 'Order Paid', order: updatedOrder });
     } else {
       res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Data.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.status,
+        email_address: req.body.email_address,
+      };
+      const updatedOrder = await order.save();
+      res.send({ message: 'Order Paid', order: updatedOrder });
     }
   })
 );
