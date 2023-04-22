41a0c997fa49780af6a3df7c45532af9a922a16c
Matija2003 <matijaj@viken.no>
2023-04-22 22:16:08 +0200

    E-commerc-Create-React-App

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,23 +8,6 @@ productRouter.get('/', async (req, res) => {
   const products = await Product.find();
   res.send(products);
 });
-
-productRouter.get('/slug/:slug', async (req, res) => {
-  const product = await Product.findOne({ slug: req.params.slug });
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-productRouter.get('/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
 const PAGE_SIZE = 3;
 productRouter.get(
   '/search',
@@ -103,12 +86,28 @@ productRouter.get(
     });
   })
 );
-
 productRouter.get(
   '/categories',
   expressAsyncHandler(async (req, res) => {
-    const categories = await Product.findOne().distinct('category');
+    const categories = await Product.find().distinct('category');
     res.send(categories);
   })
 );
+productRouter.get('/slug/:slug', async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+productRouter.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
 export default productRouter;
